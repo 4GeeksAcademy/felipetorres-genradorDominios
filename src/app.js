91dacd0f841abb87d426fa38bf05ae996c11c929
SrTorres07 <srtorresfelipe07@outlook.com>
2025-01-08 20:01:56 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let tittles = ["the", "our", "I am", "you"];
+  let adjectives = ["great", "big", "amazing", "spectacular"];
+  let heroes = ["chimichangas", "Deadpool", "Batman", "Wolverine"];
+  let extensions = [".com", ".net", ".us", ".oi"];
+
+  for (let i = 0; i < tittles.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < heroes.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          console.log(tittles[i] + adjectives[j] + heroes[k] + extensions[l]);
+        }
+      }
+    }
+  }
 };
